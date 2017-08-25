5b6529e95d44a1efb62ee4a416c9dbe22b5f8b94
Valentin Clement <clementval@gmail.com>
2017-08-25 17:53:28 +0200

    Read from module

--- fortran/src/m_smartcarb_oae.f90
+++ fortran/src/m_smartcarb_oae.f90
@@ -1,27 +1,58 @@
 !
-! 
+!
 !
 
 
-MODULE smartcarb_oae
+MODULE m_smartcarb_oae
   USE netcdf
 
   IMPLICIT NONE
 
+  ! Constant variable
+  INTEGER, PARAMETER :: nlevel = 16
+
+  character (len = *), parameter :: vertical_profile_nc = "../data/vertical_profile.nc"
+
+  ! Vertical profile values
+  REAL, DIMENSION(nlevel) :: &
+    layer_bot,               &
+    layer_top,               &
+    factor_area,             &
+    factor_point
 
 CONTAINS
 
-  SUBROUTINE read_vertical_profile
+  SUBROUTINE read_vertical_profile()
+    INTEGER :: ncid, varid, err_status
 
+    CALL ncdf_call_and_check_status(nf90_open(vertical_profile_nc, NF90_NOWRITE, ncid))
+    CALL ncdf_call_and_check_status(nf90_inq_varid(ncid, "layer_bot", varid))
+    CALL ncdf_call_and_check_status(nf90_get_var(ncid, varid, layer_bot))
+    CALL ncdf_call_and_check_status(nf90_inq_varid(ncid, "layer_top", varid))
+    CALL ncdf_call_and_check_status(nf90_get_var(ncid, varid, layer_top))
+    CALL ncdf_call_and_check_status(nf90_inq_varid(ncid, "factor_area", varid))
+    CALL ncdf_call_and_check_status(nf90_get_var(ncid, varid, factor_area))
+    CALL ncdf_call_and_check_status(nf90_inq_varid(ncid, "factor_point", varid))
+    CALL ncdf_call_and_check_status(nf90_get_var(ncid, varid, factor_point))
+    CALL ncdf_call_and_check_status(nf90_close(ncid))
   END SUBROUTINE read_vertical_profile
 
-  SUBROUTINE read_temporal_profile
+  SUBROUTINE ncdf_call_and_check_status(status)
+    INTEGER, INTENT(IN) :: status
+
+    IF (status /= nf90_noerr) THEN
+      print *, trim(nf90_strerror(status))
+      stop "NetCDF error"
+    END IF
+  END SUBROUTINE ncdf_call_and_check_status
+
+  SUBROUTINE read_temporal_profile()
 
   END SUBROUTINE read_temporal_profile
 
-  SUBROUTINE read_gridded_emissions
+  SUBROUTINE read_gridded_emissions()
 
   END SUBROUTINE read_gridded_emissions
 
 
-END MODULE smartcarb_oae
+END MODULE m_smartcarb_oae
--- fortran/test.f90
+++ fortran/test.f90
@@ -1,33 +1,9 @@
-program test 
+program test
   use netcdf
+  use m_smartcarb_oae
   implicit none
 
-  integer, parameter :: nlevel = 16
-  character (len = *), parameter :: vertical_profile_nc = "../data/vertical_profile.nc"
-  real, dimension(nlevel) :: layer_bot, layer_top, factor_area, factor_point
-  integer :: ncid, varid, err_status
-
-  layer_bot(:) = 0.0
-  layer_top(:) = 0.0
-  factor_area(:) = 0.0
-  factor_point(:) = 0.0
-
-
-  err_status =  nf90_open(vertical_profile_nc, NF90_NOWRITE, ncid)
-
-  err_status =  nf90_inq_varid(ncid, "layer_bot", varid)
-  err_status =  nf90_get_var(ncid, varid, layer_bot)
-
-  err_status =  nf90_inq_varid(ncid, "layer_top", varid)
-  err_status =  nf90_get_var(ncid, varid, layer_top)
-
-  err_status =  nf90_inq_varid(ncid, "factor_area", varid)
-  err_status =  nf90_get_var(ncid, varid, factor_area)
-
-  err_status =  nf90_inq_varid(ncid, "factor_point", varid)
-  err_status =  nf90_get_var(ncid, varid, factor_point)
-
-  err_status =  nf90_close(ncid)
+  call read_vertical_profile()
 
   print*,'SMARTCARB OAE TESTS'
   print*,layer_bot

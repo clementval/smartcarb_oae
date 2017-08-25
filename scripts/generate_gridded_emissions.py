48213453952b9a8880f7d22ee336b6578bfe6316
Valentin Clement <clementval@gmail.com>
2017-08-25 17:00:48 +0200

    Add script to generate structure of the NetCDF file for gridded 2d emissions

---
+++ scripts/generate_gridded_emissions.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from netCDF4 import Dataset
+import numpy as np
+
+dataset = Dataset('data/emissions.nc', 'w', format='NETCDF4_CLASSIC')
+
+dim_rlon = dataset.createDimension('rlon', 700) # Dummy size
+dim_rlat = dataset.createDimension('rlat', 800) # Dummy size
+
+v_rlon = dataset.createVariable('rlon', np.float64, ('rlon',))
+v_rlon.axis = 'X'
+v_rlon.units = 'degrees'
+v_rlon.standard_name = 'longitude'
+
+v_rlat = dataset.createVariable('rlat', np.float64, ('rlat',))
+v_rlat.axis = 'Y'
+v_rlat.units = 'degrees'
+v_rlat.standard_name = 'latitude'
+
+X1_01_area = dataset.createVariable('X1_01_area', np.float64, ('rlon', 'rlat',))
+X1_01_area.fill_value = -9999.0
+X1_01_area.units = 'kg h-1 cell-1'
+
+X1_01_point = dataset.createVariable('X1_01_point', np.float64, ('rlon', 'rlat',))
+X1_01_point.fill_value = -9999.0
+X1_01_point.units = 'kg h-1 cell-1'
+
+X1_02_area = dataset.createVariable('X1_02_area', np.float64, ('rlon', 'rlat',))
+X1_02_area.fill_value = -9999.0
+X1_02_area.units = 'kg h-1 cell-1'
+
+X1_02_point = dataset.createVariable('X1_02_point', np.float64, ('rlon', 'rlat',))
+X1_02_point.fill_value = -9999.0
+X1_02_point.units = 'kg h-1 cell-1'
+
+dataset.close()

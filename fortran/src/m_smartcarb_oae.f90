59bf66fd9eef96831ef13e85fd5d2117020976da
Valentin Clement <clementval@gmail.com>
2017-08-25 17:43:38 +0200

    Read vert profile

---
+++ fortran/src/m_smartcarb_oae.f90
@@ -0,0 +1,27 @@
+!
+! 
+!
+
+
+MODULE smartcarb_oae
+  USE netcdf
+
+  IMPLICIT NONE
+
+
+CONTAINS
+
+  SUBROUTINE read_vertical_profile
+
+  END SUBROUTINE read_vertical_profile
+
+  SUBROUTINE read_temporal_profile
+
+  END SUBROUTINE read_temporal_profile
+
+  SUBROUTINE read_gridded_emissions
+
+  END SUBROUTINE read_gridded_emissions
+
+
+END MODULE smartcarb_oae
--- fortran/test.f90
+++ fortran/test.f90
@@ -5,19 +5,36 @@ program test
   integer, parameter :: nlevel = 16
   character (len = *), parameter :: vertical_profile_nc = "../data/vertical_profile.nc"
   real, dimension(nlevel) :: layer_bot, layer_top, factor_area, factor_point
-  integer :: ncid, varid
+  integer :: ncid, varid, err_status
 
-  call nf90_open(vertical_profile_nc, NF90_NOWRITE, ncid)
-  call nf90_inq_varid(ncid, "layer_bot", varid)
-  call nf90_get_var(ncid, varid, layer_bot)
-  call nf90_inq_varid(ncid, "layer_top", varid)
-  call nf90_get_var(ncid, varid, layer_bot)
-  call nf90_inq_varid(ncid, "factor_area", varid)
-  call nf90_get_var(ncid, varid, layer_bot)
-  call nf90_inq_varid(ncid, "factor_point", varid)
-  call nf90_get_var(ncid, varid, layer_bot)
+  layer_bot(:) = 0.0
+  layer_top(:) = 0.0
+  factor_area(:) = 0.0
+  factor_point(:) = 0.0
+
+
+  err_status =  nf90_open(vertical_profile_nc, NF90_NOWRITE, ncid)
+
+  err_status =  nf90_inq_varid(ncid, "layer_bot", varid)
+  err_status =  nf90_get_var(ncid, varid, layer_bot)
+
+  err_status =  nf90_inq_varid(ncid, "layer_top", varid)
+  err_status =  nf90_get_var(ncid, varid, layer_top)
+
+  err_status =  nf90_inq_varid(ncid, "factor_area", varid)
+  err_status =  nf90_get_var(ncid, varid, factor_area)
+
+  err_status =  nf90_inq_varid(ncid, "factor_point", varid)
+  err_status =  nf90_get_var(ncid, varid, factor_point)
+
+  err_status =  nf90_close(ncid)
 
   print*,'SMARTCARB OAE TESTS'
+  print*,layer_bot
+  print*,layer_top
+  print*,factor_area
+  print*,factor_point
+
 
 
 end program test
